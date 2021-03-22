f102005e582b32395385b3deecba9eeb13d5468b
Kevin Hoàng <binxkevin@gmx.de>
2021-03-22 13:58:52 +0100

    Fixed 'no such method error'

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -45,4 +45,6 @@ List<CategoryModel> getCategories() {
   categoryModel.imageURL =
       'https://images.unsplash.com/photo-1518770660439-4636190af475?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
   categories.add(categoryModel);
+
+  return categories;
 }

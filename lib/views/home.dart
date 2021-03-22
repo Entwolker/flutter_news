8efa9edca588ce19d6bb6a93ddcf469ed83e971e
Kevin Hoàng <binxkevin@gmx.de>
2021-03-22 13:59:44 +0100

    Moved CategoryTile

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_news/component/category_tile.dart';
+import 'package:flutter_news/helper/data.dart';
+import 'package:flutter_news/models/category_model.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +12,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categories = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    categories = getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Flutter',
+              style: TextStyle(color: Colors.blue),
+            ),
+            Text('News'),
+          ],
+        ),
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+        elevation: 0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 70,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return CategoryTile(
+                    categoryName: categories[index].categoryName,
+                    imageURL: categories[index].imageURL,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

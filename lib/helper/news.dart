001d3fa343d9d1c27448f53b4df8d8bede1ec27f
Kevin Hoàng <binxkevin@gmx.de>
2021-03-23 13:29:11 +0100

    Implmented country & newstype to url

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -1,16 +1,23 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_news/helper/country/cc_helper.dart';
+import 'package:flutter_news/helper/types/news_type.dart';
+import 'package:flutter_news/helper/types/news_type_helper.dart';
 import 'package:flutter_news/models/article_model.dart';
 import 'package:http/http.dart' as http;
+
+import 'country/country.dart';
 
 class News {
   List<ArticleModel> news = [];
 
-  Future<void> getNews() async {
+  Future<void> getNews(
+      @required Country country, @required NewsType newsType) async {
     // https://newsapi.org/v2/top-headlines?country=de&apiKey=962815b6029c4d0a8646759591182347
-
+    final String apiKey = '962815b6029c4d0a8646759591182347';
     var url = Uri.parse(
-        'https://newsapi.org/v2/top-headlines?country=de&apiKey=962815b6029c4d0a8646759591182347');
+        'https://newsapi.org/v2/${NewsTypeHelper.getString(newsType)}?country=${CCHelper.getCC(country)}&apiKey=${apiKey}');
 
     var response = await http.get(url);
 

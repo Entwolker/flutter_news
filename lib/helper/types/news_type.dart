ca177efa54317cc6db62f8f2cb3b611c6f0b87d4
Kevin Hoàng <binxkevin@gmx.de>
2021-03-23 12:16:53 +0100

    Create news_type.dart & news_type_helper.dart

---
+++ lib/helper/types/news_type.dart
@@ -0,0 +1,9 @@
+enum NewsType {
+  business,
+  entertainment,
+  general,
+  health,
+  science,
+  sports,
+  technology
+}
---
+++ lib/helper/types/news_type_helper.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_news/helper/types/news_type.dart';
+
+class NewsTypeHelper {
+  static String getString(NewsType type) {
+    switch (type) {
+      case NewsType.business:
+        return NewsType.business.toString();
+      case NewsType.entertainment:
+        return NewsType.entertainment.toString();
+      case NewsType.general:
+        return NewsType.general.toString();
+      case NewsType.health:
+        return NewsType.health.toString();
+      case NewsType.science:
+        return NewsType.science.toString();
+      case NewsType.sports:
+        return NewsType.sports.toString();
+      case NewsType.technology:
+        return NewsType.technology.toString();
+      default:
+        return 'top-headlines';
+    }
+  }
+}
